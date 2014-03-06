b3c00282d86980af717a2e850ab2238adced304b
z3nder@gmail.com <z3nder@gmail.com@811a6f94-05ed-a85d-1ea4-e7764adca608>
2014-03-06 21:31:24 +0000

    Adding Renderer and SDL to the engine

--- src/SDLEngine/SDLEngine/SDLEngine.cpp
+++ src/SDLEngine/SDLEngine/SDLEngine.cpp
@@ -5,17 +5,36 @@ SDLEngine* SDLEngine::instance = nullptr;
 SDLEngine::SDLEngine()
 :isInitialized(false)
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING))
+	{
+		std::cout << SDL_GetError() << std::endl;
+	}
 
+	window = SDL_CreateWindow("Hello World"
+		, SDL_WINDOWPOS_CENTERED
+		, SDL_WINDOWPOS_CENTERED
+		, SCREEN_WIDTH
+		, SCREEN_HEIGHT
+		, SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS);
+	if (window == nullptr)
+	{
+		std::cout << SDL_GetError() << std::endl;
+	}
+
+	renderer = new SDLRenderer(window);
 }
 
 SDLEngine::~SDLEngine()
 {
-
+	delete renderer;
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 void SDLEngine::Start()
 {
-	for (auto iter = SDLComponent::allComponents.begin();
+	std::vector<SDLComponent*>::iterator iter;
+	for (iter = SDLComponent::allComponents.begin();
 		iter != SDLComponent::allComponents.end();
 		iter++)
 	{
@@ -29,13 +48,31 @@ void SDLEngine::Start()
 
 void SDLEngine::Run()
 {
-	while (isInitialized)
-	{
-		if (GetAsyncKeyState(VK_RETURN))
-		{
-			break;
-		}
+	SDL_Event e;
+	bool isRunning = true;
 
+	while (isRunning)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			switch (e.type)
+			{
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN:
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					isRunning = false;
+					break;
+				default:
+					break;
+				}
+			default:
+				break;
+			}
+		}
 		Update();
 		Draw();
 	}
@@ -54,12 +91,16 @@ void SDLEngine::Update()
 
 void SDLEngine::Draw()
 {
+	renderer->Clear();
+
 	for (auto iter = SDLComponent::allComponents.begin();
 		iter != SDLComponent::allComponents.end();
 		iter++)
 	{
 		(*iter)->Draw();
 	}
+
+	renderer->Present();
 }
 
 void SDLEngine::Stop()
@@ -70,4 +111,6 @@ void SDLEngine::Stop()
 	{
 		(*iter)->Stop();
 	}
+
+	Kill();
 }
