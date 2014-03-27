08355a3c8eb2a9aebf8d9ae040e1dc798ede1972
z3nder@gmail.com <z3nder@gmail.com@811a6f94-05ed-a85d-1ea4-e7764adca608>
2014-03-27 18:29:35 +0000

    SDLSprite Rendering

--- src/SDLEngine/SDLEngine/SDLSprite.cpp
+++ src/SDLEngine/SDLEngine/SDLSprite.cpp
@@ -2,6 +2,35 @@
 
 SDLSprite::SDLSprite() 
 	: SDLComponent()
+	, tex(nullptr)
+	, x(0)
+	, y(0)
+{
+}
+
+SDLSprite::SDLSprite(std::string fileName): SDLSprite(fileName, 0, 0){ }
+SDLSprite::SDLSprite(std::string fileName, int x, int y)
+	: SDLComponent()
+	, tex(nullptr)
+	, x(x)
+	, y(y)
+{
+	tex = SDLResources::GetInstance()->GetTexture(fileName);
+}
+
+SDLSprite::SDLSprite(SDL_Texture* tex)
+	: SDLComponent()
+	, tex(tex)
+	, x(0)
+	, y(0)
+{
+}
+
+SDLSprite::SDLSprite(SDL_Texture* tex, int x, int y)
+: SDLComponent()
+, tex(tex)
+, x(x)
+, y(y)
 {
 }
 
@@ -19,8 +48,18 @@ void SDLSprite::Update()
 
 void SDLSprite::Draw()
 {
+	ApplySurface(this->x, this->y, tex, SDLEngine::GetInstance()->GetRenderer());
 }
 
 void SDLSprite::Stop()
 {
+}
+
+void SDLSprite::ApplySurface(int x, int y, SDL_Texture* texture, SDL_Renderer* renderer)
+{
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	SDL_QueryTexture(texture, 0, 0, &rect.w, &rect.h);
+	SDL_RenderCopy(renderer, texture, 0, &rect);
 }
--- src/SDLEngine/SDLEngine/SDLSprite.h
+++ src/SDLEngine/SDLEngine/SDLSprite.h
@@ -1,17 +1,29 @@
 #pragma once
-
 #include "SDLComponent.h"
+#include "SDLResources.h"
 
 class SDLSprite :
 	public SDLComponent
 {
 public:
 	SDLSprite();
+	SDLSprite(std::string fileName);
+	SDLSprite(std::string fileName, int x, int y);
+	SDLSprite(SDL_Texture* tex);
+	SDLSprite(SDL_Texture* tex, int x, int y);
+
 	virtual ~SDLSprite();
 
 	void Start();
 	void Update();
 	void Draw();
 	void Stop();
+
+private:
+
+	void ApplySurface(int x, int y, SDL_Texture* texture, SDL_Renderer* renderer);
+
+	SDL_Texture* tex;
+	int x, y;
 };
 
